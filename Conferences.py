0a0995a5198457b3a41863a3915bfff346362ed1
guido1992 <rathkealex@gmail.com>
2021-11-11 22:50:30 +0000

    Add files via upload

--- Conferences.py
+++ Conferences.py
@@ -12,7 +12,19 @@ def app():
     st.write('A page to find the latest information on GAA analysis conferences')
     
     st.write("""
+             ### 2021 GAA Performance Analysis Seminar
              
+             The 2021 seminar takes place in Carlow IT on Saturday, November 20th, 2021.
+             
+             'The purpose of the seminar is to recognise and certify the latest cohort of GAA 
+             accredited performance analysts as well to:
+             * Provide an opportunity for GAA analysts to share practice and learn from each other
+             * To foster a culture of continuous support within the community of GAA analysts and 
+             beyond'
+             
+             To book your place, please click the following link.
+             
+             https://eventgen.ie/gaa-performance-analysis-seminar-2021
              
              
              """)
