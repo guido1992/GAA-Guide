a03cb2be74741781fe28afe7647e2adc2ae161e5
guido1992 <rathkealex@gmail.com>
2021-11-11 22:51:03 +0000

    Add files via upload

---
+++ Videos.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 22:00:45 2021
+
+@author: Alex
+"""
+
+# Videos.py
+import streamlit as st
+def app():
+    st.title('Video Presentations')
+    st.write('A page to find the latest video presentations on GAA analysis research & topics')
+    
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### Stats Life - Performance Analysis in the GAA
+             * What is involved in the life as a performance analyst:
+             https://www.rte.ie/sport/gaa/2019/1128/1095873-stats-life-performance-analysis-in-the-gaa/
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### GAA Coach Webinar Series 2020 - Johnny Bradley - Performance Analysis
+             * https://www.youtube.com/watch?v=pLFwKwbAFi4&ab_channel=GAALearning
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### Donegal GAA Senior Footballer's Coach Karl Lacey discusses performance analysis
+             * https://www.youtube.com/watch?v=XaE7JvLxgDU&ab_channel=AnalysisPro
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### Derry GAA Performance Analysis Workflows with Nacsport by Ben McGuckin
+             * https://www.youtube.com/watch?v=zHwFKcLQl1M&ab_channel=AnalysisPro
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+             
+    # Body text
+    st.write("""
+             ##### Creating a performance analysis template for coaches with Thomas Mount
+             * https://www.youtube.com/watch?v=pGgvm3G6ylo&ab_channel=GAALearning
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### Donegal v Monaghan Statistical Analysis
+             * https://ulster.gaa.ie/2016/06/donegal-v-monaghan-statistical-analysis/
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+             
+    # Body text
+    st.write("""
+             ##### Be Ready to Play: Performance Analysis - Moving from Insight to Action
+             * https://www.youtube.com/watch?v=cVfPFrcAUhM&ab_channel=GAALearning
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### GAA Coach Webinar Series 2020 - Gerard O Connor - Gaelic Football Transition from Defence to Attack
+             * https://www.youtube.com/watch?v=LUjuelzuY80&ab_channel=GAALearning
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+             
+    # Body text
+    st.write("""
+             ##### Analysis of Inter County Play by Rob Carroll
+             * https://www.youtube.com/watch?v=ApkUOsnB8Ec&ab_channel=GAALearning
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+             
+    # Body text
+    st.write("""
+             ##### Making Stats Work in a Club Setting
+             * https://www.youtube.com/watch?v=R4KIwEi6uUE&ab_channel=LimerickGAACoaching
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+    
+    # Body text
+    st.write("""
+             ##### A Games Centred Approach Developing Decision Makers - Paul Kinnerk
+             * https://www.youtube.com/watch?v=YQgl7qyrSAw&ab_channel=MunsterGAA
+             """)
+             
+    # Line break
+    st.write("""
+             """)
+             
+             
