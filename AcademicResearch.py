2deb61e93fb5330dabd3d4bc8ee9ac58ab4e4df4
guido1992 <rathkealex@gmail.com>
2021-11-11 22:53:28 +0000

    Add files via upload

--- AcademicResearch.py
+++ AcademicResearch.py
@@ -20,79 +20,86 @@ def app():
              the community.
              """)
     
-    ### Header - Current Ranking System
+    # Line break
+    st.write("""
+             """)
+             
+    # Header Title
+    st.write("""
+             ### Body composition
+             """)
+    
+    # Body text
     st.write("""
              ##### Body composition analysis of inter-county Gaelic athletic association players measured by dual energy X-ray absorptiometry
-             
              * https://www.tandfonline.com/doi/abs/10.1080/02640414.2015.1085076
              """)
-    
-    ### Header - Current Ranking System
-    st.write("""
-             ##### An evaluation of Gaelic Athletic Association (GAA) athletes’ self-reported practice of playing while concussed, knowledge about and attitudes towards sports-related concussion
-             
-             * https://www.degruyter.com/document/doi/10.1515/ijamh-2015-0084/html
-             """)
-    
-    ### Header - Current Ranking System
+    # Body text
     st.write("""
              ##### Seasonal changes in body composition of inter-county Gaelic Athletic Association hurlers
-             
              * https://www.tandfonline.com/doi/abs/10.1080/02640414.2016.1272709
              """)
-    
-    ### Header - Current Ranking System
-    st.write("""
-             ##### The Effects of the Gaelic Athletic Association 15 Training Program on Neuromuscular Outcomes in Gaelic Football and Hurling Players: A Randomized Cluster Trial
              
-             * https://journals.lww.com/nsca-jscr/FullText/2017/08000/The_Effects_of_the_Gaelic_Athletic_Association_15.9.aspx
+    # Line break
+    st.write("""
              """)
              
-    ### Header - Current Ranking System
+    # Header Title
     st.write("""
-             ##### Considerations and Guidelines on Athletic Development for Youth Gaelic Athletic Association Players
-             
-             * https://www.researchgate.net/profile/John-Duggan-5/publication/349213526_Considerations_and_guidelines_on_Athletic_Development_for_Youth_GAA_Players/links/6092a96da6fdccaebd0db5bd/Considerations-and-guidelines-on-Athletic-Development-for-Youth-GAA-Players.pdf
+             ### Injury Management
              """)
     
-    ### Header - Current Ranking System
+    # Body text
+    st.write("""
+             ##### An evaluation of Gaelic Athletic Association (GAA) athletes’ self-reported practice of playing while concussed, knowledge about and attitudes towards sports-related concussion
+             * https://www.degruyter.com/document/doi/10.1515/ijamh-2015-0084/html
+             """)
+    # Body text         
     st.write("""
              ##### Characteristics of potential concussive events in elite male gaelic football players: A descriptive video-analysis 
-             
              * https://www.tandfonline.com/doi/abs/10.1080/02640414.2021.1896455
              """)
-    
-    ### Header - Current Ranking System
-    st.write("""
-             ##### Entrepreneurship in an Amateur World: The Gaelic Athletic Association in Ireland
-             
-             * https://www.tandfonline.com/doi/abs/10.1080/09523367.2018.1551207
-             """)
-    
-    ### Header - Current Ranking System
-    st.write("""
-             ##### The Creation of Multimedia Resources to support the Gaelic Athletic Association (GAA) Coach Education Programme (CEP)
-             
-             * https://sciendo.com/pdf/10.1515/ijtr-2016-0011
-             """)
-             
-    ### Header - Current Ranking System
+    # Body text         
     st.write("""
              ##### Frequent but limited assessment of potentially concussed players in Gaelic Football: an opportunity to learn from other sports
-             
              * https://link.springer.com/article/10.1007/s11845-020-02390-5
              """)
-    
-    ### Header - Current Ranking System
+             
+    # Line break
+    st.write("""
+             """)
+             
+    # Header Title
+    st.write("""
+             ### Athletic & Personal Development
+             """)
+    # Body text
+    st.write("""
+             ##### The Effects of the Gaelic Athletic Association 15 Training Program on Neuromuscular Outcomes in Gaelic Football and Hurling Players: A Randomized Cluster Trial
+             * https://journals.lww.com/nsca-jscr/FullText/2017/08000/The_Effects_of_the_Gaelic_Athletic_Association_15.9.aspx
+             """)
+    # Body text         
+    st.write("""
+             ##### Considerations and Guidelines on Athletic Development for Youth Gaelic Athletic Association Players
+             * https://www.researchgate.net/profile/John-Duggan-5/publication/349213526_Considerations_and_guidelines_on_Athletic_Development_for_Youth_GAA_Players/links/6092a96da6fdccaebd0db5bd/Considerations-and-guidelines-on-Athletic-Development-for-Youth-GAA-Players.pdf
+             """)
+    # Body text
     st.write("""
              ##### Entrepreneurship in an Amateur World: The Gaelic Athletic Association in Ireland
-             
              * https://www.tandfonline.com/doi/abs/10.1080/09523367.2018.1551207
              """)
-    
-    ### Header - Current Ranking System
+    # Body text
+    st.write("""
+             ##### The Creation of Multimedia Resources to support the Gaelic Athletic Association (GAA) Coach Education Programme (CEP)
+             * https://sciendo.com/pdf/10.1515/ijtr-2016-0011
+             """)
+    # Body text         
+    st.write("""
+             ##### Entrepreneurship in an Amateur World: The Gaelic Athletic Association in Ireland
+             * https://www.tandfonline.com/doi/abs/10.1080/09523367.2018.1551207
+             """)
+    # Body text
     st.write("""
              ##### The amateur-professional debate: an exploration of attitudes and opinions within the Gaelic Athletic Association 
-             
              * https://ulir.ul.ie/handle/10344/3475
              """)
